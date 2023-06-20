49828e36bc4fc3bfc8332d5e4abe74278b66391b
jeanmichelthompson <jmtokx@gmail.com>
2023-06-20 15:40:23 -0400

    minor adjustments to resume

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
+import { ResumeComponent } from './resume/resume.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'work', loadChildren: () => import('./work/work.module').then(m => m.WorkModule) },
-  { path: 'resume', loadChildren: () => import('./resume/resume.module').then(m => m.ResumeModule) },
+  { path: 'resume', component: ResumeComponent },
   { path: 'contact', component: ContactComponent },
 ];
 
