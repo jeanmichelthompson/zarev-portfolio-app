8ac9cbac798004bc84187eb1837bcbc37cb6f8cf
jeanmichelthompson <jmtokx@gmail.com>
2023-06-22 17:18:59 -0400

    removed env variable

--- src/app/youtube.service.ts
+++ src/app/youtube.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map, of } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
-  private apiKey = environment.youtubeApiKey;
+  private apiKey = 'AIzaSyAnBRedmGaKDo-f73XxolmSVzuaIjkTKM0';
   private apiUrl = 'https://www.googleapis.com/youtube/v3/videos';
   public videoStatsCache: { [videoId: string]: any } = {};
 
